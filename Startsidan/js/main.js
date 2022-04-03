1c4fd52d1e4a6d5efe17e879de2c5a1f385ac529
Sofilus <sofia00olsson@gmail.com>
2022-04-03 23:53:26 +0200

    Working hamburger menu

---
+++ Startsidan/js/main.js
@@ -0,0 +1,7 @@
+const nav2 = document.querySelector(".nav2")
+const nav = document.querySelector("#nav")
+
+document.querySelector("#menubtn").addEventListener("click", () => {
+    nav2.classList.toggle("active");
+    nav.classList.toggle("active");
+})
